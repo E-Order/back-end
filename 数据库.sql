171ae40edbfdd9c4ea92a667afc6742a21942f56
south270 <634838533@qq.com>
2018-05-25 22:35:22 +0800

    Update 数据库.sql

--- 数据库.sql
+++ 数据库.sql
@@ -14,18 +14,20 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema mydb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `e-order` DEFAULT CHARACTER SET utf8mb4 ;
+CREATE SCHEMA IF NOT EXISTS `e-order` DEFAULT CHARACTER SET utf8mb4;
 USE `e-order` ;
 
 -- -----------------------------------------------------
 -- Table `mydb`.`ProductCategory`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `e-order`.`ProductCategory` (
-  `categoryId` INT NOT NULL,
-  `categoryName` VARCHAR(64) NOT NULL,
-  `categoryType` INT NOT NULL,
-  PRIMARY KEY (`categoryId`),
-  INDEX `index_category_type` (`categoryType` ASC))
+CREATE TABLE IF NOT EXISTS `e-order`.`product_category` (
+  `category_id` INT NOT NULL auto_increment,
+  `category_name` VARCHAR(64) NOT NULL,
+  `category_type` INT NOT NULL,
+  `create_time` timestamp not null default current_timestamp comment '创建时间',
+  `update_time` timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
+  PRIMARY KEY (`category_id`),
+  INDEX `index_category_type` (`category_type` ASC))
 ENGINE = InnoDB;
 
 
@@ -40,11 +42,13 @@ CREATE TABLE IF NOT EXISTS `e-order`.`product_info` (
   `product_icon` VARCHAR(512) NULL,
   `product_stock` INT NOT NULL DEFAULT 0,
   `category_type` INT NOT NULL,
+  `create_time` timestamp not null default current_timestamp comment '创建时间',
+  `update_time` timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
   PRIMARY KEY (`product_id`),
   INDEX `category_type_idx` (`category_type` ASC),
   CONSTRAINT `category_type`
     FOREIGN KEY (`category_type`)
-    REFERENCES `mydb`.`ProductCategory` (`categoryId`)
+    REFERENCES `e-order`.`product_category` (`category_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -58,8 +62,10 @@ CREATE TABLE IF NOT EXISTS `e-order`.`order_master` (
   `desk_id` INT NOT NULL,
   `buyer_openid` VARCHAR(64) NOT NULL,
   `order_amount` DECIMAL(8,2) NOT NULL,
-  `order_status` TINYINT(3) NOT NULL,
+  `order_status` TINYINT(3) NOT NULL DEFAULT 0,
   `pay_status` TINYINT(3) NOT NULL DEFAULT 0,
+  `create_time` timestamp not null default current_timestamp comment '创建时间',
+  `update_time` timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
   PRIMARY KEY (`order_id`),
   INDEX `index_buyer_openid` (`buyer_openid` ASC))
 ENGINE = InnoDB;
@@ -72,18 +78,20 @@ CREATE TABLE IF NOT EXISTS `e-order`.`order_detail` (
   `detail_id` VARCHAR(32) NOT NULL,
   `order_id` VARCHAR(32) NOT NULL,
   `product_quantity` INT NOT NULL,
-  `product_info_product_id` VARCHAR(32) NOT NULL,
+  `product_id` VARCHAR(32) NOT NULL,
+  `create_time` timestamp not null default current_timestamp comment '创建时间',
+  `update_time` timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
   PRIMARY KEY (`detail_id`),
   INDEX `index_order_id` (`order_id` ASC),
-  INDEX `fk_order_detail_product_info1_idx` (`product_info_product_id` ASC),
+  INDEX `index_product_id` (`product_id` ASC),
   CONSTRAINT `order_id`
     FOREIGN KEY (`order_id`)
-    REFERENCES `mydb`.`order_master` (`order_id`)
+    REFERENCES `e-order`.`order_master` (`order_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_order_detail_product_info1`
-    FOREIGN KEY (`product_info_product_id`)
-    REFERENCES `mydb`.`product_info` (`product_id`)
+  CONSTRAINT `product_id`
+    FOREIGN KEY (`product_id`)
+    REFERENCES `e-order`.`product_info` (`product_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
